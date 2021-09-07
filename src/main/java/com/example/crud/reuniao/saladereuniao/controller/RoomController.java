033dd7d5381dd90c7a29329c2309b31f7ca9b382
Ayalos <reginaldoayalos0@gmail.com>
2021-09-07 00:44:00 -0300

    Criação RoomController
    Criação e configuração

--- src/main/java/com/example/crud/reuniao/saladereuniao/controller/RoomController.java
+++ src/main/java/com/example/crud/reuniao/saladereuniao/controller/RoomController.java
@@ -1,4 +1,55 @@
 package com.example.crud.reuniao.saladereuniao.controller;
 
+import com.example.crud.reuniao.saladereuniao.exceptions.ResourceNotFoundException;
+import com.example.crud.reuniao.saladereuniao.repository.RoomRepository;
+import com.example.crud.reuniao.saladereuniao.model.Room;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+@RestController @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1")
+
 public class RoomController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    public ResponseEntity<Room> getRoomById(@PathVariable(value = "id") long roomId) throws ResourceNotFoundException {
+        {
+            Room room = roomRepository.findById(roomId)
+                    .orElseThrow(()-> new ResourceNotFoundException("Room not found!" + roomId));
+            return ResponseEntity.ok().body(room);
+        }
+    }
+
+    @PostMapping("/rooms")
+    public Room createRoom(@Valid @RequestBody Room room){
+        return roomRepository.save(room);
+    }
+
+    public ResponseEntity<Room> updateRoom(@PathVariable (value = "id") Long roomId,
+                                           @Valid @ResquestBody Room roomDetails) throws ResourceNotFoundException{
+
+    }
 }
